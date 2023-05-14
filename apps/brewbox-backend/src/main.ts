d7c4bfa8bef97a26dfab6a431878ff769701dd32
Owen Evans <owen@bgeek.net>
2023-05-14 21:20:46 +1200

    allow ssh

--- apps/brewbox-backend/src/main.ts
+++ apps/brewbox-backend/src/main.ts
@@ -7,16 +7,48 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions = null;
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    httpsOptions = {
+      key: fs.readFileSync(
+        path.join(process.cwd(), '.mkcert-ssh-cert/dev.pem')
+      ),
+      cert: fs.readFileSync(
+        path.join(process.cwd(), '.mkcert-ssh-cert/cert.pem')
+      ),
+    };
+  }
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+    cors: true,
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const ipAddresses = Object.values(os.networkInterfaces()).reduce(
+    (r, list) => {
+      return r.concat(
+        list.reduce(
+          (rr, i) =>
+            rr.concat((i.family === 'IPv4' && !i.internal && i.address) || []),
+          []
+        )
+      );
+    },
+    []
   );
+  ipAddresses.forEach((ip) => {
+    Logger.log(
+      `🚀 Application is running on: https://${ip}:${port}/${globalPrefix}`
+    );
+  });
 }
 
 bootstrap();
